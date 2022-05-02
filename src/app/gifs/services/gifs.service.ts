523c82ead60c2e2b3e8f93033f77117568afe0dc
Erick Guzman <guzmi.dev@gmail.com>
2022-05-01 21:51:27 -0600

    :art: gifs http

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Gif, SearchGifsResponse } from '../interfaces/gifs.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -6,9 +9,13 @@ import { Injectable } from '@angular/core';
 export class GifsService {
   private _historial: string[] = [];
 
+  public resultados: Gif[] = [];
+
   get historial() {
     return [...this._historial];
   }
+
+  constructor(private http: HttpClient) {}
 
   buscarGifs(query: string = ''): void {
     query = query.trim().toLowerCase();
@@ -16,5 +23,14 @@ export class GifsService {
     if (this._historial.includes(query)) return;
     this._historial.unshift(query);
     this._historial = this._historial.splice(0, 10);
+
+    this.http
+      .get<SearchGifsResponse>(
+        `https://api.giphy.com/v1/gifs/search?api_key=${environment.apiKey}&q=${query}&limit=10`
+      )
+      .subscribe((resp) => {
+        console.log(resp.data);
+        this.resultados = resp.data;
+      });
   }
 }
